4a8cb19f2d93063296dd60626f3710e6a41fb898
Jaroslaw Olszewski <oljar@tlen.pl>
2018-03-03 08:21:52 +0100

    Basic version

---
+++ WebApplication19/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication19.Models;
+
+namespace WebApplication19.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kontakt()
+
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Wykres()
+
+        {
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Parameters parameters)
+
+
+        {
+
+
+           
+
+            int n = parameters.Typoszereg + parameters.Wymiennik + parameters.Montaz + parameters.WydNom;    //  n odpowiada za wybór  centrali
+
+
+
+            if (n == 4 || n == 14)                                                                          //  zmiana dla modeli 800
+            {
+                n = n + 1;
+            }
+
+
+            int a = parameters.Sprez;
+
+            int b = parameters.Wydatek;
+
+            
+
+            ViewBag.Sprez = a;
+            ViewBag.Wydatek = b;
+            ViewBag.Centrala = n;
+
+            return View("Wykres",parameters);
+        }
+
+    }
+
+}
+
+
+
+
+
+        
---
+++ WebApplication19/Models/Parameters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication19.Models
+{
+    public class Parameters
+    {
+        public int Wydatek { get; set; }
+        public int Sprez { get; set; }
+        public int Centrala { get; set; }
+        public int Typoszereg { get; set; }
+        public int Wymiennik { get; set; }
+        public int WydNom { get; set; }
+        public int Montaz { get; set; }
+
+
+
+
+
+
+
+    }
+
+
+    
+}
