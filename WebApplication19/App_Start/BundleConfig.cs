c7c3f2839d6ed7ee37597b5c01c146c320256d3b
Jaroslaw Olszewski <oljar@tlen.pl>
2018-03-26 23:11:06 +0200

    menu 01

--- WebApplication19/App_Start/BundleConfig.cs
+++ WebApplication19/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace WebApplication19
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/Menu.css",
+                      "~/Content/Stripe.css"));
         }
     }
 }
